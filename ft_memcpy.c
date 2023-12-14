88b86e6c6f2507ec2efac55f7f7229f81706d877
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-12-14 13:46:24 +0100

    update: memcpy uses just pointers instead of pointers and indexes

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -12,21 +12,35 @@
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t bytes_copied)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	idx;
 	char	*char_dest;
 	char	*char_src;
 
 	if (dest == NULL && src == NULL)
 		return (NULL);
-	idx = 0;
 	char_dest = (char *)dest;
 	char_src = (char *)src;
-	while (idx < bytes_copied)
-	{
-		char_dest[idx] = char_src[idx];
-		idx++;
-	}
+	while (n--)
+		*char_dest++ = *char_src++;
 	return (dest);
 }
+
+// void	*ft_memcpy(void *dest, const void *src, size_t n)
+// {
+// 	size_t	idx;
+// 	char	*char_dest;
+// 	char	*char_src;
+//
+// 	if (dest == NULL && src == NULL)
+// 		return (NULL);
+// 	idx = 0;
+// 	char_dest = (char *)dest;
+// 	char_src = (char *)src;
+// 	while (idx < n)
+// 	{
+// 		char_dest[idx] = char_src[idx];
+// 		idx++;
+// 	}
+// 	return (dest);
+// }
