3939e4fa20faf30c69d53d3ed2e3bdce35921417
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-12-15 02:55:51 +0100

    improve: optimized memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,20 +14,17 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t				idx;
 	const unsigned char	*a;
 	const unsigned char	*b;
 
 	if (!n)
 		return (0);
-	idx = 0;
-	a = s1;
-	b = s2;
-	while (idx < n - 1)
+	a = (unsigned char *)s1;
+	b = (unsigned char *)s2;
+	while (--n)
 	{
 		if (*a != *b)
 			break ;
-		idx++;
 		a++;
 		b++;
 	}
